69b14cfd7754a60bffdce6ca6a63b40d6882a6e1
Maquine <mcmaquine@gmail.com>
2022-05-31 10:45:42 -0400

    Started Repo

---
+++ cam10.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cam=cv2.VideoCapture(0)
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 65, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 35, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 240, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+kernel=np.ones((5,5),np.uint8)
+while (True):
+    _, frame=cam.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    rangomax=np.array([50,255,50])
+    rangomin=np.array([0,51,0])
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+
+    mascara = cv2.inRange(frame, l_b, u_b)
+   
+    opening=cv2.morphologyEx(mascara, cv2.MORPH_OPEN, kernel)
+    x,y,w,h=cv2.boundingRect(opening)
+    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+    cv2.circle(frame,(x+w//2,y+h//2),5,(0,0,255),-1)
+    res = cv2.bitwise_and(frame, frame, mask=mascara)
+    cv2.imshow('camera',frame)
+
+    cv2.imshow('opening',opening)
+    cv2.imshow("res", res)
+    k=cv2.waitKey(1)&0xFF
+    if k==27:
+        break
+cam.release()
+cv2.destroyAllWindows()
---
+++ opcv.py
@@ -0,0 +1,85 @@
+from ctypes import sizeof
+from pickletools import uint8
+import cv2
+import numpy as np
+
+pixelHSV = np.zeros((1,3), np.int_)
+
+# mouse callback function
+def mouse_callback(event,x,y,flags,param):
+    global ix, iy, count, pixelHSV
+    if event == cv2.EVENT_LBUTTONDOWN:
+        ix = x
+        iy = y
+    
+    elif event == cv2.EVENT_LBUTTONUP:
+        print("Start X:{} End X:{} Start Y:{} End Y:{}".format(ix, x, iy, y))
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        count = 0
+        h = []
+        s = []
+        v = []
+        for i in range(ix, x+1):
+            for j in range(iy, y+1):
+                h.append( hsv[j,i][0] )
+                s.append( hsv[j,i][1] )
+                v.append( hsv[j,i][2] )
+            
+        LH = min(h)
+        UH = max(h)
+        LS = min(s)
+        US = max(s)
+        LV = min(v)
+        UV = max(v)
+
+        print("LH:{}\tUH:{}\nLS:{}\tUS:{}\nLV:{}\tUV:{}".format(LH, UH,\
+            LS, US, LV, UV))
+
+        
+
+
+# To read image from disk, we use
+# cv2.imread function, in below method,
+cam = cv2.VideoCapture(0)
+
+# To hold the window on screen, we use cv2.waitKey method
+# Once it detected the close input, it will release the control
+# To the next line
+# First Parameter is for holding screen for specified milliseconds
+# It should be positive integer. If 0 pass an parameter, then it will
+# hold the screen until user close it.
+
+while (True):
+
+    notUsed , frame = cam.read()
+ 
+    # Creating GUI window to display an image on screen
+    # first Parameter is windows title (should be in string format)
+    # Second Parameter is image array
+    cv2.imshow("Video", frame )
+    cv2.setMouseCallback("Video", mouse_callback )
+
+    key = cv2.waitKey(1)&0xFF
+    if key ==27:
+        break
+ 
+# It is for removing/deleting created GUI window from screen
+# and memory
+cam.release()
+cv2.destroyAllWindows()
+
+# Versoes anteriores
+ #elif event == cv2.EVENT_LBUTTONUP:
+  #      print("Start X:{} End X:{} Start Y:{} End Y:{}".format(ix, x, iy, y))
+   #     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #    pixelHSV = np.zeros((1,3), np.int_)
+     #   count = 0
+  #      for i in range(ix, x+1):
+   #         for j in range(iy, y+1):
+    #            print(hsv[j,i])
+     #           pixelHSV = pixelHSV + hsv[j,i]
+      #          count += 1
+        
+      #  pixelHSV = pixelHSV/count
+       # print("Media HSV")
+        #print(pixelHSV.astype(int))
